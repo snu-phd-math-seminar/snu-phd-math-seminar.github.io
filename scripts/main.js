b038ab9cd299cd571d15a1dec96ccf77b3ed9435
as197-dsa <as197@snu.edu.in>
2026-06-17 02:55:56 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -414,7 +414,7 @@ async function loadSeminarData() {
             pastContainer.insertBefore(temp.firstElementChild, showMoreBtn);
           });
           if (window.MathJax) {
-            await MathJax.typesetPromise(inserted);
+            MathJax.typesetPromise(inserted);
           }
           showMoreBtn.remove();
         });
